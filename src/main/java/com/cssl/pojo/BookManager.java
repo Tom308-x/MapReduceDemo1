b4ecc16cb0f849c38283be204ae8b92d9e70e7d3
honlyshit <1225784272@qq.com>
2021-10-11 12:15:32 +0800

    添加pojo

--- src/main/java/com/cssl/pojo/BookManager.java
+++ src/main/java/com/cssl/pojo/BookManager.java
@@ -1,10 +1,14 @@
 package com.cssl.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookManager {
 private Integer id;
 private String name;
