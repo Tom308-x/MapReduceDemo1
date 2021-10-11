e1c244b5953dab281f855ad9fd786b16e35c5a8f
Tom308-x <1844282367@qq.com>
2021-10-11 14:31:41 +0800

    swagger

--- src/main/java/com/cssl/BookmanagerSysApplication.java
+++ src/main/java/com/cssl/BookmanagerSysApplication.java
@@ -1,9 +1,11 @@
 package com.cssl;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cssl.mapper")
 public class BookmanagerSysApplication {
 
     public static void main(String[] args) {
