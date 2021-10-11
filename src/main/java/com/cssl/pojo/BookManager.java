eb3e6d0ddbe4c6f8d194dc5832a714bf5a75b0ae
NeOoooooL <510378612@qq.com>
2021-10-11 14:30:05 +0800

    html

--- src/main/java/com/cssl/pojo/BookManager.java
+++ src/main/java/com/cssl/pojo/BookManager.java
@@ -3,6 +3,7 @@ package com.cssl.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -13,6 +14,7 @@ public class BookManager {
 private Integer id;
 private String name;
 private String author;
+@DateTimeFormat(pattern = "yyyy-MM-dd")
 private Date time;
 private Integer type;
 }
